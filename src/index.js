43b5d3ab979847a6ce399e8ae3e71146739ec4dd
javierovico <javierovico@gmail.com>
2023-06-28 15:47:42 -0400

    reformat

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ import * as cocoSsd from '@tensorflow-models/coco-ssd';
 
 // Convert image from file system to JavaScript object
 import * as images from '../images'
+
 const parseImageImport = (images) => {
-    // Use local image, 
+    // Use local image,
     let imageObject = {}
     const localImg = new Image()
-  
+
     for (let key in images) {
-      const name = `${key}Src`
-      const tempImg = new Image()
-      tempImg.src = images[key]
-      imageObject = {...imageObject, [`${key}Src`]: tempImg}
+        const name = `${key}Src`
+        const tempImg = new Image()
+        tempImg.src = images[key]
+        imageObject = {...imageObject, [`${key}Src`]: tempImg}
     }
     return imageObject
 }
@@ -22,41 +23,62 @@ const parseImageImport = (images) => {
 // Detect contour, and draw box around same
 const detectContour = (img, context, predictions) => {
     predictions.forEach((prediction) => {
-      context.font = "20px Arial";
-      context.beginPath()
-      context.rect(...prediction.bbox)
-      context.lineWidth = 5;
-      context.strokeStyle = 'red';
-      context.fillStyle = 'white';
-      context.stroke();
+        context.font = "20px Arial";
+        context.beginPath()
+        context.rect(...prediction.bbox)
+        context.lineWidth = 5;
+        context.strokeStyle = 'red';
+        context.fillStyle = 'white';
+        context.stroke();
 
-      // Draw text
-      context.fillText(`${prediction.class} - ${prediction.score.toFixed(3)}`, prediction.bbox[0], prediction.bbox[1]+10)
+        // Draw text
+        context.fillText(`${prediction.class} - ${prediction.score.toFixed(3)}`, prediction.bbox[0], prediction.bbox[1] + 10)
     })
 
 }
 
 // Draw image
 const drawImage = (predictions, image, index, canvas) => {
-  const context = canvas.getContext('2d')
-  context.drawImage(image,0,0)
-  canvas.addEventListener('click',() => detectContour(image, context, predictions))
+    const context = canvas.getContext('2d')
+    /*
+    // Calcula las dimensiones de la imagen escalada
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+    const imageAspect = image.width / image.height;
+    let drawWidth = canvasWidth;
+    let drawHeight = canvasHeight;
+
+    if (imageAspect > 1) {
+        drawHeight = drawWidth / imageAspect;
+    } else {
+        drawWidth = drawHeight * imageAspect;
+    }
+
+    // Calcula la posición de la imagen escalada para centrarla en el canvas
+    const offsetX = (canvasWidth - drawWidth) / 2;
+    const offsetY = (canvasHeight - drawHeight) / 2;
+
+    // Dibuja la imagen escalada en el canvas
+    context.drawImage(image, offsetX, offsetY, drawWidth, drawHeight);
+     */
+    context.drawImage(image, 0, 0)
+    canvas.addEventListener('click', () => detectContour(image, context, predictions))
 }
 
 (async () => {
-  const imageObject = parseImageImport(images)
+    const imageObject = parseImageImport(images)
 
-  Object.entries(imageObject).forEach(([,img], index) => {
-    const canvas = document.createElement('canvas');
-    canvas.setAttribute("width",'600')
-    canvas.setAttribute("height",'600')
-    canvas.setAttribute("src","./src/index.js")
-    canvas.setAttribute("class","canvas")
-    document.body.appendChild(canvas);
+    Object.entries(imageObject).forEach(([, img], index) => {
+        const canvas = document.createElement('canvas');
+        canvas.setAttribute("width", '600')
+        canvas.setAttribute("height", '600')
+        canvas.setAttribute("src", "./src/index.js")
+        canvas.setAttribute("class", "canvas")
+        document.body.appendChild(canvas);
 
-    cocoSsd.load().then((model) => 
-    model.detect(img).then((predictions) => {
-      drawImage(predictions, img, index, canvas)
-    }))
-  })
+        cocoSsd.load().then((model) =>
+            model.detect(img).then((predictions) => {
+                drawImage(predictions, img, index, canvas)
+            }))
+    })
 })()
